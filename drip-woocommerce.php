15ef65c6a58ee4bd8d556dfb80cc9ee555bd7256
Andrew Tongen <andrew.tongen@drip.com>
2020-02-07 09:22:25 -0600

    Render drip js snippet if account_id is present

--- drip-woocommerce.php
+++ drip-woocommerce.php
@@ -7,9 +7,14 @@
 
 defined( 'ABSPATH' ) || die( 'Executing outside of the WordPress context.' );
 
+require_once(__DIR__ . '/src/class-drip-woocommerce-settings.php');
+require_once(__DIR__ . '/src/class-drip-woocommerce-snippet.php');
+
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ), true ) ) {
     require_once __DIR__ . '/src/class-drip-woocommerce-cart-events.php';
-    include(__DIR__ . '/src/class-drip-woocommerce-settings.php');
+
+    WC_Settings_Drip::init();
+    WC_Snippet_Drip::init();
 
     (new Drip_Woocommerce_Cart_Events())->setup_cart_actions();
 }
