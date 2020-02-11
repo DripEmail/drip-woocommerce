131379c50431cdea58469523269083dd28418021
Thom Dieterich <thom.dieterich@drip.com>
2020-02-11 10:14:21 -0600

    pass json instead of object to do_action

--- src/class-drip-woocommerce-cart-events.php
+++ src/class-drip-woocommerce-cart-events.php
@@ -35,7 +35,7 @@ class Drip_Woocommerce_Cart_Events
             }
         }
         
-        do_action( 'wc_drip_woocommerce_cart_event', $event_data );
+        do_action( 'wc_drip_woocommerce_cart_event', json_encode( $event_data ) );
 
         if(WC()->cart->is_empty()) {
             $this->remove_drip_cart_session_id();
@@ -45,7 +45,7 @@ class Drip_Woocommerce_Cart_Events
     private function base_event() {
         WC()->cart->calculate_totals();
         return array(
-            'event_action' => self::CART_UPDATED_ACTION,
+            'event_action' => self::CART_UPDATED_ACTION, // TODO: we can trap cart created events if we have to generate a new cart session id
             'customer_email' => wp_get_current_user()->user_email,
             'session' => $this->drip_cart_session_id(),
             'grand_total' => WC()->cart->get_total('drip_woocommerce'),
@@ -64,18 +64,18 @@ class Drip_Woocommerce_Cart_Events
 
         if( ! $product_data ) { return false; }
         return array(
-            "product_id" => $cart_item_info['product_id'],
-            "product_variant_id" => $product_key,
-            "sku" => $product_data->get_sku('drip_woocommerce'),
-            "name" => $product_data->get_name('drip_woocommerce'),
-            "short_description" => $product_data->get_short_description('drip_woocommerce'),
-            "price" => $product_data->get_price('drip_woocommerce'),
-            "taxes" => $cart_item_info['line_tax'],
-            "total" => $cart_item_info['line_total'],
-            "quantity" => $cart_item_info['quantity'],
-            "product_url" => $product_data->get_permalink(),
-            "image_url" => $product_data->get_image('woocommerce_single', array(), true),
-            "categories" => $this->product_categories($product_data)
+            'product_id' => $cart_item_info['product_id'],
+            'product_variant_id' => $product_key,
+            'sku' => $product_data->get_sku('drip_woocommerce'),
+            'name' => $product_data->get_name('drip_woocommerce'),
+            'short_description' => $product_data->get_short_description('drip_woocommerce'),
+            'price' => $product_data->get_price('drip_woocommerce'),
+            'taxes' => $cart_item_info['line_tax'],
+            'total' => $cart_item_info['line_total'],
+            'quantity' => $cart_item_info['quantity'],
+            'product_url' => $product_data->get_permalink(),
+            'image_url' => $product_data->get_image('woocommerce_single', array(), true),
+            'categories' => $this->product_categories($product_data)
         );
     }
 
