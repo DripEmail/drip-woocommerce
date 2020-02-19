f090bf31a5ec6ac244788a31a4a1b71ac4f43d91
Andrew Tongen <andrew.tongen@drip.com>
2020-02-19 10:22:20 -0600

    Only update header on webhooks named "drip"

--- src/class-drip-woocommerce-version.php
+++ src/class-drip-woocommerce-version.php
@@ -6,14 +6,16 @@ class Drip_Woocommerce_Version {
   const HEADER = "X-WC-Drip-Version";
 
   public static function init() {
-    add_filter( "woocommerce_webhook_http_args", __CLASS__ . "::add_version_header", 50 );
+    add_filter( "woocommerce_webhook_http_args", __CLASS__ . "::add_version_header", 50, 3 );
   }
 
-  public static function add_version_header( $http_args ) {
-    // TODO: Is it possible to only set this for webhooks destined to drip?
-    // if $webhook->get_name() contains 'Drip', perhaps?
-    if ( $version = self::my_plugin_data( "Version" ) ) {
-      $http_args["headers"][self::HEADER] = $version;
+  public static function add_version_header( $http_args, $_arg, $this_id ) {
+    // $this_id is webhook id
+    $webhook_name = (new WC_Webhook($this_id))->get_name();
+    if ( strpos(strtolower($webhook_name), "drip") !== false ) {
+      if ( $version = self::my_plugin_data( "Version" ) ) {
+        $http_args["headers"][self::HEADER] = $version;
+      }
     }
     return $http_args;
   }
