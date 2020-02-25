b736578a79d9b95a5d03f20eef4306211186807a
Andrew Tongen <andrew.tongen@drip.com>
2020-02-25 16:17:19 -0600

    Add release script

---
+++ bin/release.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+dir=${PWD##*/}
+
+if [[ ! -z "$(git status --porcelain)" ]]; then
+  >&2 echo "ERROR: your working copy is dirty"
+  exit 1
+fi
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$branch" != "master" ]]; then
+  >&2 echo "ERROR: you are not on the master branch"
+  exit 1
+fi
+
+ghr=$(which ghr)
+if [[ -z "$ghr" ]]; then
+  >&2 echo "ERROR: ghr not found in PATH, please install: https://github.com/tcnksm/ghr"
+  exit 1
+fi
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  >&2 echo "ERROR: GITHUB_TOKEN env var not set, this is required to upload release to github"
+  exit 1
+fi
+
+plugin_version=$(cat drip-woocommerce.php | grep -e '^Version: ' | awk '{print $2}')
+if [[ -z "$plugin_version" ]]; then
+  >&2 echo "ERROR: unable to parse plugin version from drip-woocommerce.php"
+  exit 1
+fi
+
+echo "Using version from plugin metadata: $plugin_version"
+
+git fetch origin
+
+if git tag | grep -q "$plugin_version"; then
+  >&2 echo "ERROR: tag $plugin_version already exists!"
+  exit 1
+fi
+
+# create and push tag
+
+git tag -s $plugin_version -m "Release $plugin_version"
+git push origin $plugin_version
+
+# generate release zip and upload to github
+
+[[ -d dist ]] && rm -rf dist # ensure old dist is not uploaded
+mkdir -p dist
+trap "rm -f dist" EXIT
+zipball=$(bin/make_zipball.sh | tail -1)
+mv $zipball dist/drip-woocommerce-${plugin_version}.zip
+$ghr $plugin_version dist
