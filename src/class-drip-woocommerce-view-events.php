e8aea03db6d17673d6c30f5ba771c404bf72012e
Ian Nance <iantnance@gmail.com>
2020-02-26 11:05:41 -0800

    better way to include script

--- src/class-drip-woocommerce-view-events.php
+++ src/class-drip-woocommerce-view-events.php
@@ -16,32 +16,23 @@ class Drip_Woocommerce_View_Events
     }
 
     public function drip_woocommerce_viewed_product() {
+      wp_register_script( 'Drip product view tracking', plugin_dir_url( __FILE__ ) . 'product_view_tracking.js' );
       $product = wc_get_product();
-      $currency = get_option('woocommerce_currency');
-      $categories = $this->product_categories($product);
-      $image_id  = $product->get_image_id();
-      $image_url = wp_get_attachment_image_url( $image_id );
-      $script_tag = <<<TAG
-        <!-- Drip track -->
-        <script type="text/javascript">
-        var _dcq = _dcq || [];
-        _dcq.push(["track", "Viewed a product", {
-            product_id: "{$product->get_id('drip_woocommerce')}",
-            product_variant_id: "{$product->get_variation_id('drip_woocommerce')}",
-            sku: "{$product->get_sku('drip_woocommerce')}",
-            name: "{$product->get_name('drip_woocommerce')}",
-            categories: "{$categories}",
-            price: "{$product->get_price('drip_woocommerce')}",
-            currency: "{$currency}",
-            product_url: "{$product->get_permalink()}",
-            image_url: "{$image_url}",
-            source: "drip_woocommerce"
-        }]);
-        </script>
-        <!-- end Drip track -->
-TAG;
+      $product_attributes = array(
+        "product_id" => $product->get_id('drip_woocommerce'),
+        "product_variant_id" => $product->get_variation_id('drip_woocommerce'),
+        "sku" => $product->get_sku('drip_woocommerce'),
+        "name" => $product->get_name('drip_woocommerce'),
+        "price" => $product->get_price('drip_woocommerce'),
+        "product_url" => $product->get_permalink(),
+        "currency" => get_option('woocommerce_currency'),
+        "categories" => $this->product_categories($product),
+        "image_id"  => $product->get_image_id(),
+        "image_url" => wp_get_attachment_image_url( $image_id )
+      );
 
-      echo $script_tag;
+      wp_localize_script( 'Drip product view tracking', 'product', $product_attributes );
+      wp_enqueue_script('Drip product view tracking', plugin_dir_url( __FILE__ ) . 'product_view_tracking.js', array(), 1.0, true);
     }
 
     private function product_categories($product) {
@@ -50,6 +41,6 @@ TAG;
             $woo_category = get_term_by( 'id', absint( $cid ), 'product_cat' );
             array_push($categories, $woo_category->name);
         }
-        return join(",", $categories);
+        return join(',', $categories);
     }
 }
