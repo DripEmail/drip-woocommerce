98e615d4be0603e317048d9b81763bf0c7994c9c
William Johnston <william.johnston@drip.com>
2020-02-27 16:10:26 -0600

    Linter fixes for snippet

--- src/snippet.js.php
+++ src/snippet.js.php
@@ -1,13 +1,22 @@
+<?php
+/**
+ * The Drip JS snippet
+ *
+ * @package Drip_Woocommerce
+ */
+
+?>
+
 <!-- Drip Code -->
 <script type="text/javascript">
 	var _dcq = _dcq || [];
 	var _dcs = _dcs || {};
-	_dcs.account = '<?php echo $account_id ?>';
+	_dcs.account = '<?php echo esc_js( $account_id ); ?>';
 
 	(function() {
 		var dc = document.createElement('script');
 		dc.type = 'text/javascript'; dc.async = true;
-		dc.src = '//tag.getdrip.com/<?php echo $account_id ?>.js';
+		dc.src = '//tag.getdrip.com/<?php echo esc_js( $account_id ); ?>.js';
 		var s = document.getElementsByTagName('script')[0];
 		s.parentNode.insertBefore(dc, s);
 	})();
