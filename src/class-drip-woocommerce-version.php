4ac15b005c4ab0492535d99a804a3937db493415
William Johnston <william.johnston@drip.com>
2020-02-27 16:49:08 -0600

    Clean up version component

--- src/class-drip-woocommerce-version.php
+++ src/class-drip-woocommerce-version.php
@@ -1,28 +1,51 @@
 <?php
+/**
+ * Injects Drip plugin version header into relevant webhooks.
+ *
+ * @package Drip_Woocommerce
+ */
 
-require_once( ABSPATH . "wp-admin/includes/plugin.php" );
+require_once ABSPATH . 'wp-admin/includes/plugin.php';
 
+/**
+ * Injects Drip plugin version header into relevant webhooks.
+ */
 class Drip_Woocommerce_Version {
-	const HEADER = "X-WC-Drip-Version";
+	const HEADER = 'X-WC-Drip-Version';
 
+	/**
+	 * Set up component
+	 */
 	public static function init() {
-		add_filter( "woocommerce_webhook_http_args", __CLASS__ . "::add_version_header", 50, 3 );
+		add_filter( 'woocommerce_webhook_http_args', __CLASS__ . '::add_version_header', 50, 3 );
 	}
 
+	/**
+	 * Callback for woocommerce_webhook_http_args filter
+	 *
+	 * @param  array  $http_args argument for the http request.
+	 * @param  mixed  $_arg      unused.
+	 * @param string $this_id   the ID of the webhook.
+	 */
 	public static function add_version_header( $http_args, $_arg, $this_id ) {
-		// $this_id is webhook id
-		$webhook_name = (new WC_Webhook($this_id))->get_name();
-		if ( strpos(strtolower($webhook_name), "drip") !== false ) {
-			if ( $version = self::my_plugin_data( "Version" ) ) {
-				$http_args["headers"][self::HEADER] = $version;
+		$webhook_name = ( new WC_Webhook( $this_id ) )->get_name();
+		if ( strpos( strtolower( $webhook_name ), 'drip' ) !== false ) {
+			$version = self::my_plugin_data( 'Version' );
+			if ( $version ) {
+				$http_args['headers'][ self::HEADER ] = $version;
 			}
 		}
 		return $http_args;
 	}
 
+	/**
+	 * Get data out of plugin frontmatter.
+	 *
+	 * @param string $attribute a plugin attribute.
+	 */
 	private static function my_plugin_data( $attribute ) {
-		$path = realpath(__DIR__ . "/../drip-woocommerce.php");
+		$path        = realpath( __DIR__ . '/../drip-woocommerce.php' );
 		$plugin_data = get_plugin_data( $path );
-		return trim($plugin_data[$attribute]);
+		return trim( $plugin_data[ $attribute ] );
 	}
 }
