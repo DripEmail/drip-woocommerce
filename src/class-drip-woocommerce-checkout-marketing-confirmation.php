ab06aadac4201833220cd41434dfd6c11ff1d4e0
William Johnston <william.johnston@drip.com>
2020-03-09 14:51:14 -0500

    Initial implementation

---
+++ src/class-drip-woocommerce-checkout-marketing-confirmation.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Insert checkbox on checkout
+ *
+ * @package Drip_Woocommerce
+ */
+
+defined( 'ABSPATH' ) || die( 'Executing outside of the WordPress context.' );
+
+/**
+ * Insert checkbox on checkout
+ */
+class Drip_Woocommerce_Checkout_Marketing_Confirmation {
+	const FIELD_NAME = 'drip_woocommerce_accepts_marketing';
+
+	/**
+	 * Set up component
+	 */
+	public static function init() {
+		$marketing_confirmation = new self();
+		$marketing_confirmation->setup_actions();
+	}
+
+	/**
+	 * Whether the customer chose to accept marketing
+	 *
+	 * @var bool
+	 */
+	private $accepts_marketing;
+
+	/**
+	 * Initialize actions/filters
+	 */
+	public function setup_actions() {
+		add_action( 'woocommerce_review_order_before_submit', array( $this, 'callback_review_order' ), 10, 0 );
+		add_action( 'woocommerce_checkout_order_processed', array( $this, 'callback_checkout_order_processed' ), 10, 3 );
+	}
+
+	/**
+	 * Callback for woocommerce_review_order_before_submit
+	 */
+	public function callback_review_order() {
+		woocommerce_form_field(
+			self::FIELD_NAME,
+			array(
+				'type'  => 'checkbox',
+				// TODO: Make this configurable.
+				'label' => 'Subscribe to newsletter',
+			)
+		);
+	}
+
+	/**
+	 * Callback for woocommerce_checkout_process
+	 *
+	 * @param int      $order_id The ID of the generated order.
+	 * @param array    $posted_data The data posted to the form.
+	 * @param WC_Order $order The order object.
+	 */
+	public function callback_checkout_order_processed( $order_id, $posted_data, $order ) {
+		// Ignoring nonce since it was checked earlier.
+		// phpcs:ignore WordPress.Security.NonceVerification.Missing
+		if ( isset( $_POST[ self::FIELD_NAME ] ) && '1' === $_POST[ self::FIELD_NAME ] ) {
+			do_action(
+				// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
+				'wc_drip_woocommerce_accepts_marketing',
+				array(
+					'email'             => $order->get_billing_email(),
+					'accepts_marketing' => true,
+				)
+			);
+		}
+	}
+}
