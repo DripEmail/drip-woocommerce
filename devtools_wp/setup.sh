3f748d885517072f4c638a6c7762d5151d92fee9
Will Strinz <will.strinz@drip.com>
2023-09-01 12:59:00 -0500

    use traefik

--- devtools_wp/setup.sh
+++ devtools_wp/setup.sh
@@ -5,28 +5,28 @@ set -e
 # Add --build when you need to rebuild the Dockerfile.
 docker-compose up -d --build
 
-port=$(docker-compose port web 80 | cut -d':' -f2)
+# port=$(docker-compose port web-woodev 80 | cut -d':' -f2)
 
 # Wait for the DB to be up.
 docker-compose exec -T db /bin/bash -c 'while ! mysql --protocol TCP -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "show databases;" > /dev/null 2>&1; do sleep 1; done'
 
 woocommerce_setup_script=$(cat <<SCRIPT
 cd /var/www/html/ && \
-/usr/local/bin/wp core install --url="http://localhost:3007" --title="drip_woocommerce_test" --admin_user="drip" --admin_email="drip@example.com" --admin_password="abc1234567890" --skip-email && \
+/usr/local/bin/wp core install --url="http://woodev.drip.local" --title="drip_woocommerce_test" --admin_user="drip" --admin_email="drip@example.com" --admin_password="abc1234567890" --skip-email && \
 /usr/local/bin/wp option set woocommerce_feature_custom_order_tables_enabled yes && \
 /usr/local/bin/wp plugin activate woocommerce && \
 /usr/local/bin/wp plugin activate drip; \
 /usr/local/bin/wp media import https://cdn3.iconfinder.com/data/icons/nature-emoji/50/Forest-512.png
-CART_PAGE_ID=$(/usr/local/bin/wp post create --post_type=page --post_author="drip" --post_title="My Fair Cart" --post_name="My Fair Cart" --post_content="[woocommerce_cart]" --post_status="publish" --porcelain) && \
-/usr/local/bin/wp option set woocommerce_cart_page_id $CART_PAGE_ID --autoload='yes'; \
-CHKOUT_PAGE_ID=$(/usr/local/bin/wp post create --post_type=page --post_author="drip" --post_title="My Fair Checkout" --post_name="My Fair Checkout" --post_content="[woocommerce_checkout]" --post_status="publish" --porcelain) && \
-/usr/local/bin/wp option set woocommerce_checkout_page_id $CHKOUT_PAGE_ID --autoload='yes'; \
+CART_PAGE_ID=\$(/usr/local/bin/wp post create --post_type=page --post_author="drip" --post_title="My Fair Cart" --post_name="My Fair Cart" --post_content="[woocommerce_cart]" --post_status="publish" --porcelain) && \
+/usr/local/bin/wp option set woocommerce_cart_page_id \$CART_PAGE_ID --autoload='yes'; \
+CHKOUT_PAGE_ID=\$(/usr/local/bin/wp post create --post_type=page --post_author="drip" --post_title="My Fair Checkout" --post_name="My Fair Checkout" --post_content="[woocommerce_checkout]" --post_status="publish" --porcelain) && \
+/usr/local/bin/wp option set woocommerce_checkout_page_id \$CHKOUT_PAGE_ID --autoload='yes'; \
 /usr/local/bin/wp option set woocommerce_cod_settings '{"enabled":"yes","title":"Cash on delivery","description":"Pay with cash upon delivery.","instructions":"Pay with cash upon delivery.","enable_for_methods":"","enable_for_virtual":"yes"}' --autoload='yes' --format='json'; \
 /usr/local/bin/wp wc update; \
 if ! grep -q drip_woo_test_force_mocks wp-includes/functions.php; then
 cat << "EOF" >> wp-includes/functions.php
-function drip_woo_test_force_mocks($is_external, $host) {
-	return $is_external || 'mock' === $host;
+function drip_woo_test_force_mocks(\$is_external, \$host) {
+	return \$is_external || 'mock' === \$host;
 }
 add_filter( 'http_request_host_is_external', 'drip_woo_test_force_mocks', 10, 2 );
 function drip_woo_test_deliver_async() {
@@ -44,7 +44,7 @@ if ! grep -q 'composer.phar' /root/.bashrc; then
   echo 'alias composer="$composer"' >> /root/.bashrc
 fi
 if ! grep -q 'composer/vendor' /root/.bashrc; then
-  echo 'PATH=/root/.composer/vendor/bin:${PATH}' >> /root/.bashrc
+  echo 'PATH=/root/.composer/vendor/bin:\${PATH}' >> /root/.bashrc
 fi
 if ! grep -q PHP_MEMORY_LIMIT /root/.bashrc; then
   echo 'export PHP_MEMORY_LIMIT=1G' >> /root/.bashrc
@@ -52,14 +52,14 @@ fi
 SCRIPT
 )
 
-docker-compose exec -T -u root web /bin/bash -c "chown www-data wp-content"
-docker-compose exec -T -u www-data web /bin/bash -c "$woocommerce_setup_script"
-docker-compose exec -T -u root web /bin/bash -c "$(cat install-composer.sh)"
-docker-compose exec -T -u root web /bin/bash -c "$update_bashrc_script"
-docker-compose exec -T -u root web /bin/bash -c "$
+docker-compose exec -T -u root web-woodev /bin/bash -c "chown www-data wp-content"
+docker-compose exec -T -u www-data web-woodev /bin/bash -c "$woocommerce_setup_script"
+docker-compose exec -T -u root web-woodev /bin/bash -c "$(cat install-composer.sh)"
+docker-compose exec -T -u root web-woodev /bin/bash -c "$update_bashrc_script"
+docker-compose exec -T -u root web-woodev /bin/bash -c "$
 $composer config --no-plugins allow-plugins.dealerdirect/phpcodesniffer-composer-installer true
 "
-docker-compose exec -T -u root web /bin/bash -c "
+docker-compose exec -T -u root web-woodev /bin/bash -c "
   $composer global require 'squizlabs/php_codesniffer=*' && \
     $composer require --dev \
       'dealerdirect/phpcodesniffer-composer-installer:^0.7' \
